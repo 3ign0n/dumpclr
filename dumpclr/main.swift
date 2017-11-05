2df35c91b73190b11104f55e9401e88b52ee2623
TAKEDA hiroyuki(aka 3ign0n) <take6a.hir0yuki@gmail.com>
2017-11-05 17:26:07 +0900

    dump .clr file

--- dumpclr/main.swift
+++ dumpclr/main.swift
@@ -7,4 +7,38 @@
 //
 
 import Foundation
+import AppKit.NSColor
 
+let executable = URL(string: CommandLine.arguments[0])!
+
+func showHelp() {
+    print("usage:")
+    print("\(executable.lastPathComponent) <path to color palette file>")
+}
+
+func dumpColorPalette(file: URL) {
+    guard let colorList = NSColorList(name: NSColorList.Name(file.lastPathComponent), fromFile: file.path) else {
+        showHelp()
+        return
+    }
+    
+    colorList.allKeys.forEach {
+        print("\(String(describing: colorList.color(withKey: $0)))")
+    }
+}
+
+func main() {
+    guard CommandLine.argc == 2 else {
+        showHelp()
+        return
+    }
+    
+    guard let paletteFile = URL(string: CommandLine.arguments[1]), FileManager.default.fileExists(atPath: paletteFile.absoluteString) else {
+        showHelp()
+        return
+    }
+    
+    dumpColorPalette(file: paletteFile)
+}
+
+main()
