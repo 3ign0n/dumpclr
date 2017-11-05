411f7f03d14bbd85bd13abab72b23d902391a9e3
TAKEDA hiroyuki(aka 3ign0n) <take6a.hir0yuki@gmail.com>
2017-11-05 16:59:06 +0900

    Initial Commit

---
+++ dumpclr/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  dumpclr
+//
+//  Created by TAKEDA hiroyuki on 2017/11/05.
+//  Copyright © 2017 3ign0n. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
